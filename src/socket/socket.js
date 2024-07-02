1f7cbcfa803c2c99610b1f4c157e6e96b33d8bcf
rojanagunoori <nagunooriroja@gmail.com>
2024-07-02 14:13:20 +0530

    third

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL =  'http://localhost:3000'|| "https://instapostdeploy-sever.onrender.com"; // Make sure this matches your backend URL
+const URL =  'http://localhost:3000'|| "https://instapostdeploy-sever.onrender.com" || "wss://instapostdeploy-sever.onrender.com"
 const socket = io(URL);
 
 export default socket;
