fe2e74495e1770505892c4302e9de96ef15e2960
rojanagunoori <nagunooriroja@gmail.com>
2026-04-04 22:47:04 +0530

    resume chnage

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000"; //"https://instapostdeploy-sever.onrender.com"|| 'http://localhost:3000'; // Make sure this matches your backend URL
+const URL =
+  "https://instapostdeploy-sever.onrender.com" || "http://localhost:3000"; // Make sure this matches your backend URL
 const socket = io(URL);
 
 export default socket;
